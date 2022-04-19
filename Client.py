9ac2dbe2253aec8632d6b19364269b5f45ff390d
Emilie Van Bogaert <21258@ecam.be>
2022-04-19 11:40:48 +0200

    programmation de reponse basique
    programmation de réponse basique + nouveau constructor pour avoir diffèrent client

--- Client.py
+++ Client.py
@@ -3,24 +3,44 @@ import socket
 import threading as th
 
 class Client:
-    def __init__(self):
+    run = True
+
+    def __init__(self,port):
+        #rajouter exeption pour les int
         self.s = socket.socket()
         request = "subscribe"
-        port = 8888
-        name ="justUnTest"
-        matricules = ["12345","67890"]
+        name = "justUnTest"
+        matricules = ["12345", "67890"]
+        self.config(request,port,name,matricules)
+
+    def config(self,request,port, name,matricules):
         jsonDico = {"request": request, "port":port, "name":name,"matricules":matricules}
         address = ('127.0.0.1', 3000)
-        self.s.connect(address)
+        self.connect(address , jsonDico)
+
+    def connect(self,adresseServor, jsonDico):
+        self.s.connect(adresseServor)
         invitation = json.dumps(jsonDico)
         self.s.send(invitation.encode())
         threadEcoute = th.Thread(target = self.ecoute)
         threadEcoute.start()
+
     def ecoute(self):
-        reponseServor = self.s.recv(2048).decode()
-        reponse = json.loads(reponseServor)
-
-
-
+        while self.run:
+            reponseServor = self.s.recv(2048).decode()
+            #Attenstion parfois il est vide
+            if reponseServor is not '':
+                reponse = json.loads(reponseServor)
+                print(reponse)
+            else :
+                reponse = {}
+            if 'reponse' in reponse.keys():
+                if reponse['reponse'] == 'ok':
+                    pass
+                if reponse['reponse']== 'ping':
+                    self.s.send(json.dumps({'reponse':'pong'}).encode())
+            if 'request' in reponse.keys():
+                if reponse['request'] =='play':
+                    self.s.send(json.dumps({'reponse':'giveup'}).encode())
     def fin(self):
         self.s.close()
--- first.py
+++ first.py
@@ -1,6 +1,7 @@
 import Client
 if __name__=="__main__":
-    prems = Client.Client()
+    prems = Client.Client(8000)
+    dems = Client.Client(8880)
     truc = input("hello")
 
 
