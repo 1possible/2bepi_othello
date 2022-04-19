f59bacb92c4713148edb0f0fe345a62828c4b8c6
Emilie Van Bogaert <21258@ecam.be>
2022-04-19 10:45:09 +0200

    mise en place d'un thread
    essaie avec un thread

--- Client.py
+++ Client.py
@@ -1,21 +1,26 @@
 import json
 import socket
-
+import threading as th
 
 class Client:
     def __init__(self):
-        s = socket.socket()
+        self.s = socket.socket()
         request = "subscribe"
         port = 8888
         name ="justUnTest"
         matricules = ["12345","67890"]
         jsonDico = {"request": request, "port":port, "name":name,"matricules":matricules}
         address = ('127.0.0.1', 3000)
-        s.connect(address)
+        self.s.connect(address)
         invitation = json.dumps(jsonDico)
-        print(invitation)
-        s.send(invitation.encode())
-        reponse = s.recv(2048).decode()
-        print(reponse)
-        s.close()
-        print("s close")
+        self.s.send(invitation.encode())
+        threadEcoute = th.Thread(target = self.ecoute)
+        threadEcoute.start()
+    def ecoute(self):
+        reponseServor = self.s.recv(2048).decode()
+        reponse = json.loads(reponseServor)
+
+
+
+    def fin(self):
+        self.s.close()
--- first.py
+++ first.py
@@ -1,6 +1,7 @@
 import Client
 if __name__=="__main__":
     prems = Client.Client()
+    truc = input("hello")
 
 
 def add(x,y):
