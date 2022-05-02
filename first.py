c6c89760746ae5d99bcac167f864d93001d0de5b
Emilie Van Bogaert <21258@ecam.be>
2022-05-02 17:19:21 +0200

    rajout du choix de strategy

--- first.py
+++ first.py
@@ -1,10 +1,9 @@
 import AICom
 if __name__=="__main__":
-    prems = AICom.AICom(8000,"prems",["12355", "67899"])
-    dems = AICom.AICom(8880,"dems",["12345", "67890"])
-    ter = AICom.AICom(8881, "ter", ["14545", "67780"])
-    quar= AICom.AICom(8882,"quar", ["98765","14758"])
-    truc = input("hello \n")
+    prems = AICom.AICom(8000,"Debilus1",["12355", "67899"],'a')
+    dems = AICom.AICom(8880,"Debilus2",["12345", "67890"],'a')
+    #minmax1 = AICom.AICom(8881, "MinMax1", ["14545", "67780"],'m')
+    #minmax2 = AICom.AICom(8882,"MinMax2", ["98765","14758"],'m')
 
 
 def add(x,y):
