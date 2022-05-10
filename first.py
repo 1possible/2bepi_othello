dd554ebea2d6d9562eaceac436c5ff3c08f0eca7
Emilie Van Bogaert <21258@ecam.be>
2022-05-10 18:37:26 +0200

    reparer first

--- first.py
+++ first.py
@@ -1,16 +1,16 @@
 import AICom
 if __name__=="__main__":
-    prems = AICom.AICom(8000,"Debilus1",["12355", "67899"],'a')
-    dems = AICom.AICom(8881,"Debilus2",["12345", "67890"],'a')
+    prems = AICom.AICom(8000,"Debilus1",'a',["12355", "67899"])
+    dems = AICom.AICom(8881,"Debilus2",'a',["12345", "67890"])
 
-    ter = AICom.AICom(8882, "firstCoup1", ["14545", "67780"],'f')
-    quar = AICom.AICom(8883,"firstCoup2", ["98765","14758"],'f')
+    ter = AICom.AICom(8882, "firstCoup1",'f', ["14545", "67780"])
+    quar = AICom.AICom(8883,"firstCoup2",'f', ["98765","14758"])
 
-    pent = AICom.AICom(8884, "mine1", ["14555", "67555"], 'n')
-    hex = AICom.AICom(8885, "mine2", ["98666", "14666"], 'n')
+    pent = AICom.AICom(8884, "mine1", 'n', ["14555", "67555"])
+    hex = AICom.AICom(8885, "mine2", 'n', ["98666", "14666"])
 
-    roversi1= AICom.AICom(8886, "roversi1", ["98763", "14753"],'m')
-    roversi2 = AICom.AICom(8887,"roversi2",["12395", "67490"],'m')
+    roversi1= AICom.AICom(8886, "roversi1",'m', ["98763", "14753"])
+    roversi2 = AICom.AICom(8887,"roversi2",'m',["12395", "67490"])
 
-    i1 = AICom.AICom(8888, "i1", ["18763", "24753"], 'i')
-    i2 = AICom.AICom(8889, "i2", ["22395", "17490"], 'i')
+    i1 = AICom.AICom(8888, "i1", 'i', ["18763", "24753"])
+    i2 = AICom.AICom(8889, "i2", 'i', ["22395", "17490"])
